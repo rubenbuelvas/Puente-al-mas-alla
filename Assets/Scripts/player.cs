87cbea9a2ba1c11003069c081d726f5b750028d1
rubenbuelvas <rcbuelvasv@gmail.com>
2023-11-26 15:30:02 -0500

    Activate trap

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -19,15 +20,18 @@ public class player : MonoBehaviour
     public Button heavenButton;
     public Button hellButton;
     public TextMeshProUGUI descriptionText;
+    public GameObject trap;
 
     NPC currentNPC;
     Queue<NPC> NPCQueue;
+    GameObject currentTrap;
 
     void Start()
     {
         InitiateNPCQueue();
-        heavenButton.onClick.AddListener(MoveToNextNPC);
-        hellButton.onClick.AddListener(MoveToNextNPC);
+        // Differentiate heaven and hell
+        heavenButton.onClick.AddListener(SendToHeaven);
+        hellButton.onClick.AddListener(SendToHell);
         if (NPCQueue.Count > 0)
         {
             UpdateNPC();
@@ -36,6 +40,7 @@ public class player : MonoBehaviour
 
     void InitiateNPCQueue()
     {
+        currentTrap = Instantiate(trap);
         NPCQueue = new Queue<NPC>();
         for (int i = 0; i < NPCGameObjectList.Count; i++)
         {
@@ -47,10 +52,26 @@ public class player : MonoBehaviour
         }
     }
 
+    void SendToHeaven()
+    {
+        EditorUtility.DisplayDialog("Decisión!", "Enviaste a " + currentNPC.name + " al cielo", ":D", ":(");
+        MoveToNextNPC();
+    }
+
+    void SendToHell()
+    {
+        StartCoroutine(Waiter());
+    }
+
     void MoveToNextNPC()
     {
+        if (currentTrap == null)
+        {
+            currentTrap = Instantiate(trap);
+        }
         if (NPCQueue.Count == 0)
         {
+            EditorUtility.DisplayDialog("Fin", "Gracias por jugar la demo!", "ok", ":(");
             return;
         }
         Destroy(currentNPC.gameObject);
@@ -62,7 +83,21 @@ public class player : MonoBehaviour
         currentNPC = NPCQueue.Dequeue();
         currentNPC.gameObject = Instantiate(currentNPC.gameObject);
         descriptionText.text = currentNPC.description;
-        Debug.Log(currentNPC.name);
-        Debug.Log(currentNPC.description);
+        //Debug.Log(currentNPC.name);
+        //Debug.Log(currentNPC.description);
+    }
+
+    void ActivateTrap()
+    {
+        trap.transform.Rotate(new Vector3(90f, 0));
+    }
+
+    IEnumerator Waiter()
+    {
+        Destroy(currentTrap);
+        currentTrap = null;
+        yield return new WaitForSeconds(2);
+        EditorUtility.DisplayDialog("Decisión!", "Enviaste a " + currentNPC.name + " al infierno", ":D", ":(");
+        MoveToNextNPC();
     }
 }
