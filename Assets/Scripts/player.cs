5bd27277ccf65d8cbc94e3836aaaa2d05a614c6d
rubenbuelvas <rcbuelvasv@gmail.com>
2023-11-26 09:05:44 -0500

    Update NPCs with description and names

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -3,41 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public class NPC
+{
+    public GameObject gameObject;
+    public string name;
+    public string description;
+}
+
 public class player : MonoBehaviour
 {
-    public List<GameObject> NPCList;
+    public List<GameObject> NPCGameObjectList;
+    public List<string> NPCNameList;
+    public List<string> NPCDescriptionList;
     public Button heavenButton;
     public Button hellButton;
 
-    GameObject currentNPC;
-    Queue<GameObject> NPCQueue;
+    NPC currentNPC;
+    Queue<NPC> NPCQueue;
 
-
-    // Start is called before the first frame update
     void Start()
     {
-        NPCQueue = new Queue<GameObject>(NPCList);
+        InitiateNPCQueue();
         heavenButton.onClick.AddListener(MoveToNextNPC);
         hellButton.onClick.AddListener(MoveToNextNPC);
         if (NPCQueue.Count > 0)
         {
-            currentNPC = Instantiate(NPCQueue.Dequeue());
+            UpdateNPC();
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    void InitiateNPCQueue()
     {
-        
+        NPCQueue = new Queue<NPC>();
+        for (int i = 0; i < NPCGameObjectList.Count; i++)
+        {
+            NPC npc = new NPC();
+            npc.gameObject = NPCGameObjectList[i];
+            npc.name = NPCNameList[i];
+            npc.description = NPCDescriptionList[i];
+            NPCQueue.Enqueue(npc);
+        }
     }
 
     void MoveToNextNPC()
     {
-        if (NPCList.Count == 0)
+        if (NPCQueue.Count == 0)
         {
             return;
         }
-        Destroy(currentNPC);
-        currentNPC = Instantiate(NPCQueue.Dequeue());
+        Destroy(currentNPC.gameObject);
+        UpdateNPC();
+    }
+
+    void UpdateNPC()
+    {
+        currentNPC = NPCQueue.Dequeue();
+        currentNPC.gameObject = Instantiate(currentNPC.gameObject);
+        Debug.Log(currentNPC.name);
+        Debug.Log(currentNPC.description);
     }
 }
